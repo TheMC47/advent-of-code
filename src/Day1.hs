94ea7a527752a41d7f4656ce0845f8e4558b825f
Yecine Megdiche <yecine.megdiche@gmail.com>
2020-12-29 21:39:57 +0100

    Day1, Part 2

--- src/Day1.hs
+++ src/Day1.hs
@@ -2,16 +2,28 @@ module Day1 where
 
 import           Data.List
 
- -- O ( n log n ) by sorting the input and iterating
-solve :: [Int] -> Int
-solve input = let (f, s) = go 2020 sorted (reverse sorted) in f * s
+-- Part 1
+
+-- O ( n log n ) by sorting the input and iterating
+solve1 :: Int -> [Int] -> Int
+solve1 target input = let (f, s) = go sorted (reverse sorted) in f * s
  where
-  go :: Int -> [Int] -> [Int] -> (Int, Int)
-  go target (x : xs) (y : ys) | x + y == target = (x, y)
-                              | x + y < target  = go target xs (y : ys) -- up
-                              | otherwise       = go target (x : xs) ys -- down
-  go _ _ _ = (0, 0)
+  go :: [Int] -> [Int] -> (Int, Int)
+  go (x : xs) (y : ys) | x + y == target = (x, y)
+                       | x + y < target  = go xs (y : ys)
+                       | otherwise       = go (x : xs) ys
+  go _ _ = (0, 0)
   sorted = sort input
 
-day1 :: IO ()
-day1 = interact (show . solve . map read . lines)
+day1_1 :: IO ()
+day1_1 = interact (show . solve1 2020 . map read . lines)
+
+
+-- Part 2
+
+solve2 :: [Int] -> Int
+-- could be optimized by only reversing and sorting once, but I'm lazy
+solve2 xs = head . filter (/= 0) $ map (\x -> x * solve1 (2020 - x) xs) xs
+
+day1_2 :: IO ()
+day1_2 = interact (show . solve2 . map read . lines)
