7e409c47178ec569db75bf1a035b5a58fc5026b9
Yecine Megdiche <yecine.megdiche@gmail.com>
2021-12-01 23:12:43 +0100

    Solved Day1

--- AoC2021/src/Day1.hs
+++ AoC2021/src/Day1.hs
@@ -1,4 +1,15 @@
 module Day1 where
 
+import System.IO
+
+countIncreasing :: [Int] -> Int
+countIncreasing xs = length . filter (< 0) $ zipWith (-) xs (tail xs)
+
+solveDay1 :: String -> String
+solveDay1 = show . countIncreasing . map read . lines
+
 day1 :: IO ()
-day1 = print "not implemented"
+day1 = do
+  handle <- openFile "inputs/day1" ReadMode
+  contents <- hGetContents handle
+  print $ solveDay1 contents
