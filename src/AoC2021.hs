516cf1d56603bb4ddd0171808891d5df0d61e26a
Yecine Megdiche <yecine.megdiche@gmail.com>
2021-12-02 21:57:33 +0100

    Added Day1 Part2

--- src/AoC2021.hs
+++ src/AoC2021.hs
@@ -1,8 +1,8 @@
 module AoC2021 where
 
-import AoC
-import AoC2021.Day1
-import AoC2021.Day2
+import           AoC
+import           AoC2021.Day1
+import           AoC2021.Day2
 
 aoc2021 :: AoC
-aoc2021 = AoC 2021 [[day1], [day2_1, day2_2]]
+aoc2021 = AoC 2021 [[day1_1, day1_2], [day2_1, day2_2]]
--- src/AoC2021/Day1.hs
+++ src/AoC2021/Day1.hs
@@ -1,7 +1,21 @@
 module AoC2021.Day1 where
 
+import Data.List.Split
+
+
+day1 :: ([Int] -> Int) -> String -> String
+day1 f = show . f . map read . lines
+
 countIncreasing :: [Int] -> Int
 countIncreasing xs = length . filter (< 0) $ zipWith (-) xs (tail xs)
 
-day1 :: String -> String
-day1 = show . countIncreasing . map read . lines
+day1_1 :: String -> String
+day1_1 = day1 countIncreasing
+
+
+countIncreasingSlidingWindow :: [Int] -> Int
+countIncreasingSlidingWindow xs = countIncreasing
+    $ zipWith3 (\x y z -> x + y + z) xs (tail xs) (tail (tail xs))
+
+day1_2 :: String -> String
+day1_2 = day1 countIncreasingSlidingWindow
