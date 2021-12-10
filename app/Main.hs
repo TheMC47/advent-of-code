c095fbee5f047a2ebc68312717712afb6b57f8b6
Yecine Megdiche <yecine.megdiche@gmail.com>
2021-12-10 09:49:43 +0100

    Timing stuff

--- app/Main.hs
+++ app/Main.hs
@@ -4,25 +4,29 @@
 
 module Main where
 
-import AoC
-import AoC2021
-import Data.Maybe
-import Miloud
-import System.Environment
-import System.IO
-import Text.Read
+import           AoC
+import           AoC2021
+import           Data.Maybe
+import           Miloud
+import           System.Environment
+import           System.IO
+import           System.TimeIt
+import           Text.Read
 
 main :: IO ()
 main = go aoc2021
-  where
-    go AoC {..} =
-      getArgs >>= \case
-        (readMaybe -> Just day) : (readMaybe -> Just part) : (headMaybe -> fromMaybe "" -> suff)
-          | Just solver <- ((<!!> (part - 1)) =<< (days <!!> (day - 1))) -> run year solver day suff
-        (readMaybe -> Just day) : (headMaybe -> fromMaybe "" -> suff)
-          | Just solver <- (headMaybe =<< (days <!!> (day - 1))) -> run year solver day suff
-        _ -> print "Bye"
-    run year solver day suff =
-      openFile ("inputs/" <> show year <> "/day" <> show day <> suff) ReadMode
-        >>= hGetContents
-        >>= print . solver
+ where
+  go AoC {..} = getArgs >>= \case
+    (readMaybe -> Just day) : (readMaybe -> Just part) : (headMaybe -> fromMaybe "" -> suff)
+      | Just solver <- ((<!!> (part - 1)) =<< (days <!!> (day - 1)))
+      -> run year solver day suff
+    (readMaybe -> Just day) : (headMaybe -> fromMaybe "" -> suff)
+      | Just solver <- (headMaybe =<< (days <!!> (day - 1))) -> run year
+                                                                    solver
+                                                                    day
+                                                                    suff
+    _ -> print "Bye"
+  run year solver day suff =
+    openFile ("inputs/" <> show year <> "/day" <> show day <> suff) ReadMode
+      >>= hGetContents
+      >>=  timeIt . print . solver
