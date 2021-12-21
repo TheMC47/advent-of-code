cddec36dfcc6b5d81c4a5787f747543d4af260b4
Yecine Megdiche <yecine.megdiche@gmail.com>
2021-12-21 19:54:43 +0100

    Failed attempts to scale part2 for n = 1000

--- src/AoC2021/Day21.hs
+++ src/AoC2021/Day21.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
 module AoC2021.Day21 where
@@ -6,8 +7,9 @@ module AoC2021.Day21 where
 
 import           Control.Arrow
 import           Control.Monad.State
+import qualified Data.IntMap                   as IM
 import           Data.List
-import qualified Data.Map                      as M
+import qualified Data.Map.Strict               as M
 import           Data.Maybe
 import qualified Data.Vector                   as V
 
@@ -102,12 +104,7 @@ play' ps = dp V.! 0 V.! pos1_0 V.! pos2_0 V.! 0 V.! 0
   where
     pos1_0 = fromJust $ M.lookup P1 ps
     pos2_0 = fromJust $ M.lookup P2 ps
-    rolls =
-        map (\l -> (head l, genericLength l))
-            $ group
-            $ sort
-            $ [ x + y + z | x <- [1 .. 3], y <- [1 .. 3], z <- [1 .. 3] ]
-    dp = V.generate
+    dp     = V.generate
         2
         (\p -> V.generate
             11
@@ -136,11 +133,122 @@ play' ps = dp V.! 0 V.! pos1_0 V.! pos2_0 V.! 0 V.! 0
                     V.! pos2'
                     V.! score1'
                     V.! score2'
-              | (r, h) <- rolls
+              | (r, h) <- diracRolls
               ]
+
+diracRolls :: [(Int, Integer)]
+diracRolls =
+    map (head &&& genericLength)
+        $ group
+        $ sort
+        $ [ x + y + z | x <- [1 .. 3], y <- [1 .. 3], z <- [1 .. 3] ]
+
+type Game' = State (M.Map Round' (Integer, Integer))
+
+type Round' = (Int, Position, Position, Score, Score)
+
+nextPlayer :: Int -> Int
+nextPlayer 0 = 1
+nextPlayer 1 = 0
+
+play_ :: Round' -> Game' (Integer, Integer)
+play_ i@(player, pos1, pos2, score1, score2)
+    | score1 >= 21 = return (1, 0)
+    | score2 >= 21 = return (0, 1)
+    | otherwise = do
+        x <- gets (M.lookup i)
+        case x of
+            Just s  -> return s
+            Nothing -> do
+                res <- foldr sumTuple (0, 0) <$> sequence
+                    [ let
+                          pos1' = wrap (if player == 0 then pos1 + r else pos1)
+                          pos2' = wrap (if player == 1 then pos2 + r else pos2)
+                          score1' =
+                              if player == 0 then score1 + pos1' else score1
+                          score2' =
+                              if player == 1 then score2 + pos2' else score2
+                      in
+                          do
+                              !v <-
+                                  join (***) (* h)
+                                      <$> play_
+                                              ( nextPlayer player
+                                              , pos1'
+                                              , pos2'
+                                              , score1'
+                                              , score2'
+                                              )
+                              return v
+                    | (r, h) <- diracRolls
+                    ]
+                modify (M.insert i res)
+                return res
+
+type Game'' = State (IM.IntMap (Integer, Integer))
+
+play_' :: Round' -> Game'' (Integer, Integer)
+play_' i@(player, pos1, pos2, score1, score2)
+    | score1 >= 1000 = return (1, 0)
+    | score2 >= 1000 = return (0, 1)
+    | otherwise = do
+        let !h = hash i
+        x <- gets (IM.lookup h)
+        case x of
+            Just s  -> return s
+            Nothing -> do
+                !res <- foldr sumTuple (0, 0) <$> sequence
+                    [ let
+                          pos1' = wrap (if player == 0 then pos1 + r else pos1)
+                          pos2' = wrap (if player == 1 then pos2 + r else pos2)
+                          score1' =
+                              if player == 0 then score1 + pos1' else score1
+                          score2' =
+                              if player == 1 then score2 + pos2' else score2
+                      in
+                          do
+                              !v <-
+                                  join (***) (* h)
+                                      <$> play_'
+                                              ( nextPlayer player
+                                              , pos1'
+                                              , pos2'
+                                              , score1'
+                                              , score2'
+                                              )
+                              return v
+                    | (r, h) <- diracRolls
+                    ]
+                modify (IM.insert h res)
+                return res
+
+hash :: Round' -> Int
+hash (player, pos1, pos2, score1, score2) =
+    (((score2 * 10000 + score1) * 100 + pos2) * 100 + pos1) * 10 + player
+
+runPlay :: Positions -> (Integer, Integer)
+runPlay ps =
+    let pos1_0 = fromJust $ M.lookup P1 ps
+        pos2_0 = fromJust $ M.lookup P2 ps
+    in  evalState (play_ (0, pos1_0, pos2_0, 0, 0)) M.empty
+
+
+runPlay' :: Positions -> (Integer, Integer)
+runPlay' ps =
+    let pos1_0 = fromJust $ M.lookup P1 ps
+        pos2_0 = fromJust $ M.lookup P2 ps
+    in  evalState (play_' (0, pos1_0, pos2_0, 0, 0)) IM.empty
 
 sumTuple :: (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer)
 sumTuple (a, b) (c, d) = (a + c, b + d)
 
 day21_2 :: String -> String
 day21_2 = show . uncurry max . play' . parseInput
+
+
+day21_3 :: String -> String
+day21_3 = show . uncurry max . runPlay . parseInput
+
+
+day21_4 :: String -> String
+day21_4 = show . uncurry max . runPlay' . parseInput
