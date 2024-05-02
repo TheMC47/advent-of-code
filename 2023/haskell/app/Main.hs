36410eb319d090de5148d2f7609f22bd7f28a109
Yecine Megdiche <yecine.megdiche@gmail.com>
2024-05-02 22:14:57 +0200

    change main

--- 2023/haskell/app/Main.hs
+++ 2023/haskell/app/Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ViewPatterns #-}
+
 module Main where
 
 import qualified Day1 as D1
@@ -7,7 +10,12 @@ import qualified Day4 as D4
 import qualified Day5 as D5
 import qualified Day6 as D6
 import qualified Day7 as D7
+import qualified Day8 as D8
+
+import System.Environment
+import System.Exit (exitFailure)
 import System.IO (IOMode (ReadMode), hGetContents, openFile)
+import Text.Read (readMaybe)
 
 type Solver = String -> String
 
@@ -16,34 +24,42 @@ data Day = D {part1 :: !Solver, part2 :: !Solver, day :: !Int}
 input :: Day -> String
 input = ("input/day" <>) . show . day
 
-data AOC = A
-    { day1 :: !Day
-    , day2 :: !Day
-    , day3 :: !Day
-    , day4 :: !Day
-    , day5 :: !Day
-    , day6 :: !Day
-    , day7 :: !Day
-    }
+type AOC = [Day]
+
+mkAoc :: [(Solver, Solver)] -> AOC
+mkAoc = zipWith (\i (p1, p2) -> D p1 p2 i) [1 ..]
 
 aoc :: AOC
 aoc =
-    A
-        { day1 = D D1.part1 D1.part2 1
-        , day2 = D D2.part1 D2.part2 2
-        , day3 = D D3.part1 D3.part2 3
-        , day4 = D D4.part1 D4.part2 4
-        , day5 = D D5.part1 D5.part2 5
-        , day6 = D D6.part1 D6.part2 6
-        , day7 = D D7.part1 D7.part2 7
-        }
+    mkAoc
+        [ (D1.part1, D1.part2)
+        , (D2.part1, D2.part2)
+        , (D3.part1, D3.part2)
+        , (D4.part1, D4.part2)
+        , (D5.part1, D5.part2)
+        , (D6.part1, D6.part2)
+        , (D7.part1, D7.part2)
+        , (D8.part1, D8.part2)
+        ]
 
 main :: IO ()
-main = solve day7 part2
+main = do
+    args <- getArgs
+    case args of
+        [readMaybe @Int -> Just dayNumber, readMaybe @Int -> Just part] ->
+            if dayNumber > 0 && dayNumber <= length aoc
+                then case part of
+                    1 -> solve (dayNumber - 1) part1
+                    2 -> solve (dayNumber - 1) part2
+                    _ -> help
+                else help
+        _ -> help
   where
+    help = putStrLn "Usage: aoc <day> <part>" >> exitFailure
+    solve :: Int -> (Day -> Solver) -> IO ()
     solve d p = do
         let
-            theDay = d aoc
+            theDay = aoc !! d
             thePart = p theDay
             inputFile = input theDay
         contents <- hGetContents =<< openFile inputFile ReadMode
