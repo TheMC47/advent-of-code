741883a4fcc596b1fc5fe120d4a52b310c6fc9d5
Yecine Megdiche <yecine.megdiche@gmail.com>
2025-12-01 09:03:25 +0100

    day1

--- 2025/c++/src/day01.cpp
+++ 2025/c++/src/day01.cpp
@@ -1,55 +1,160 @@
-#include <charconv>
 #include <format>
+#include <fstream>
 #include <iostream>
 #include <optional>
 #include <ostream>
+#include <stdexcept>
+#include <string>
 #include <string_view>
 #include <system_error>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
-std::string part1() { return "Hello, World!"; }
+template <typename T> int sgn(T val) { return (T(0) < val) - (val < T(0)); }
 
-std::string part2() { return "Hello, World 2!"; }
-
-[[nodiscard]] std::optional<int> safeParseInt(std::string_view str) noexcept {
-    auto begin = str.data();
-    auto end = str.data() + str.size();
-    int out{};
-    auto res = std::from_chars(begin, end, out);
-    if (res.ec == std::errc{} && res.ptr == end) {
-        return out;
-    }
-    return std::nullopt;
+inline int wrap100(int x) {
+  x %= 100;
+  return x < 0 ? x += 100 : x;
 }
 
+std::pair<std::string, std::string>
+solve(const std::vector<std::string> &lines) {
+  int dial = 50;
+  int password1 = 0;
+  int password2 = 0;
+  for (const auto &line : lines) {
+    int steps;
+    auto result =
+        std::from_chars(line.data() + 1, line.data() + line.size(), steps);
+    if (result.ec != std::errc()) {
+      std::cerr << std::make_error_code(result.ec).message() << std::endl;
+      throw std::runtime_error(
+          std::format("Could not read number at line {}", line));
+    }
+
+    const int oldDial = dial;
+
+    if (line[0] == 'L') {
+      dial -= steps;
+    } else if (line[0] == 'R') {
+      dial += steps;
+    } else {
+      throw std::runtime_error(std::format("Unexpected line {}", line));
+    }
+
+    // Handle full rotations, and then check if we land on zero or we have a non
+    // full rotation
+    password2 += std::abs(dial) / 100;
+    password2 += (dial == 0 || (oldDial != 0 && sgn(oldDial) != sgn(dial)));
+
+    dial = wrap100(dial);
+
+    password1 += (dial == 0);
+  }
+  return {std::to_string(password1), std::to_string(password2)};
+}
+
+std::string readFile(std::string_view path) {
+  std::ifstream file{std::string(path)};
+  if (!file.is_open()) {
+    throw std::runtime_error("Could not open file");
+  }
+  return std::string{std::istreambuf_iterator<char>(file),
+                     std::istreambuf_iterator<char>()};
+}
+
+std::vector<std::string> readFileLines(std::string_view path) {
+  std::ifstream file{std::string(path)};
+  if (!file.is_open()) {
+    throw std::runtime_error("Could not open file");
+  }
+
+  std::vector<std::string> lines;
+  std::string line;
+  while (std::getline(file, line)) {
+    lines.push_back(line);
+  }
+  return lines;
+}
+
+class CLIArguments {
+private:
+  std::unordered_map<std::string_view, std::string_view> options{};
+  std::unordered_set<std::string_view> flags{};
+  std::vector<std::string_view> positionals{};
+
+public:
+  CLIArguments(int argc, char *argv[]) {
+    for (int i = 1; i < argc; i++) {
+      const std::string_view currStr{argv[i]};
+
+      if (currStr[0] != '-') {
+        positionals.push_back(currStr);
+        continue;
+      }
+
+      const int nextIdx = i + 1;
+      if (nextIdx == argc) {
+        flags.emplace(currStr);
+        continue;
+      }
+      const std::string_view nextStr{argv[nextIdx]};
+      if (nextStr[0] == '-') {
+        continue;
+      }
+      options.emplace(currStr, nextStr);
+      i++;
+    }
+  }
+
+  std::optional<std::string_view> getOpt(std::string_view opt) {
+    const auto it = options.find(opt);
+    if (it == options.end()) {
+      return std::nullopt;
+    }
+    return {it->second};
+  }
+
+  std::optional<std::string_view> getPositional(int idx) {
+    if (idx >= positionals.size() || idx < 0) {
+      return std::nullopt;
+    }
+    return {positionals[idx]};
+  }
+};
+
 int main(int argc, char *argv[]) {
-    auto usage = [argv]() {
-        std::cout << std::format("Usage: {} [part] (part: 1, 2)", argv[0])
-                  << std::endl;
-        return 1;
-    };
+  auto usage = [argv]() {
+    std::cout << std::format("Usage: {} <part> (part: 1, 2)", argv[0])
+              << std::endl;
+    return 1;
+  };
 
-    if (argc < 2) {
-        return usage();
-    }
+  CLIArguments args{argc, argv};
 
-    auto dayStr = argv[1];
-    const auto day = safeParseInt(dayStr);
+  const auto part = args.getPositional(0);
+  const auto runPart1 = !part.has_value() || part.value() == "1";
+  const auto runPart2 = !part.has_value() || part.value() == "2";
 
-    if (!day.has_value()) {
-        std::cout << std::format("Argument is not a number: {}", dayStr)
-                  << std::endl;
-        return usage();
-    }
-    switch (*day) {
-    case 1:
-        std::cout << part1() << std::endl;
-        break;
-    case 2:
-        std::cout << part2() << std::endl;
-        break;
-    default:
-        std::cout << std::format("Unrecognized part: {}", dayStr) << std::endl;
-        return usage();
-    }
-    return 0;
+  const auto filePath = args.getOpt("-f").value_or("inputs/day01.in");
+  const auto lines = readFileLines(filePath);
+
+  if (!runPart1 && !runPart2) {
+    std::cout << std::format("Unrecognized part: {}", part.value())
+              << std::endl;
+    return usage();
+  }
+
+  const auto [p1, p2] = solve(lines);
+
+  if (runPart1) {
+    std::cout << "Part1: " << std::endl;
+    std::cout << p1 << std::endl;
+  }
+  if (runPart2) {
+    std::cout << "Part2: " << std::endl;
+    std::cout << p2 << std::endl;
+  }
+  return 0;
 }
